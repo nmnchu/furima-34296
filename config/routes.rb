80dafc8c3c0679a820187ba669e53b204efbfd11
nmnchu <namika1218@gmail.com>
2021-05-31 23:33:03 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-  resources :users, only: [:edit, :update]
+  resources :prototypes, only: [:new, :edit, :update] do
+  end
 end
