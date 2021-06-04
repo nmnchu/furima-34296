39daf4a0aee9a46f3101ee70e3e65f77deab74a4
nmnchu <namika1218@gmail.com>
2021-06-04 23:31:52 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  validates :nickname, presence: true, length: { maximum: 20 }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :birthday, presence: true
 end
