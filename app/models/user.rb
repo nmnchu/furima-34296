3a46a9366726bf9120a29492ca16c1f0ba8e18aa
nmnchu <namika1218@gmail.com>
2021-06-06 19:33:03 +0900

    ユーザー管理機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname
+  validates :birthday
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
-
+  
   with_options presence: true do
-    validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
-    validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/, message: 'Full-width characters' } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
   end
 end
