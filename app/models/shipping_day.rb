a443bc266ef3a8397f4837fd70bc704a556eab84
nmnchu <namika1218@gmail.com>
2021-06-12 21:01:28 +0900

    ActiveHashの導入

--- app/models/shipping_day.rb
+++ app/models/shipping_day.rb
@@ -1,7 +1,7 @@
-class Genre < ActiveHash::Base
+class ShippingDay < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '1~2日で発送' },
+    { id: 2, name: '1〜2日で発送' },
     { id: 3, name: '２〜3日で発送' },
     { id: 4, name: '４〜７日で発送' },
   ]
