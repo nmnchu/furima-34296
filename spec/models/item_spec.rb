a0c504b8a1f9455c9d11b31fbb98bb886eb91425
nmnchu <namika1218@gmail.com>
2021-06-23 08:34:05 +0900

    出品機能編集

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -127,6 +127,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("User must exist")
       end
+
+      it '全角文字では登録できないこと' do
+        @item.price = '１１１'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not included in the list")
+      end
     end
   end
 end
