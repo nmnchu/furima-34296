3cd88a172781eeda190cf97f9b49ecbfdfca5851
nmnchu <namika1218@gmail.com>
2021-07-10 20:54:38 +0900

    購入機能編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :create]
+  before_action :authenticate_user!, only: [:index]
   before_action :find_item,only: [:index,:create]
   before_action :move_to_index, only: [:index, :create]
 
