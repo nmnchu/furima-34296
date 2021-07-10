4a576ec8aa99ac99c3889f30656a727d9b9be372
nmnchu <namika1218@gmail.com>
2021-07-10 18:36:05 +0900

    商品購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :find_item,only: [:index,:create]
-  before_action :move_to_index, except: [:index, :new, :create, :show]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -38,7 +38,7 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == @item.user.id
+    return redirect_to root_path if current_user.id == @item.user.id || @item.order.present?
   end
   
 
